d514b93d47b679bd15b2d55b046d38ebc8cd5201
Naima_Bak <n.bakenchich@gmail.com>
2023-04-02 02:16:04 +0200

    dashboard first design, login + register + logout => home  using sanctum auth laravel v9

---
+++ back_end/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $req)
+    {
+        $validator = Validator::make($req->all(),[
+           'name'=> 'required',
+           'email'=> 'required|email|max:190|unique:users,email',
+           'password' => 'required|min:8'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'validation_errors' => $validator->messages(),
+            ]);
+        }else{
+               $user = User::create([
+                 'name' => $req->name,
+                 'email' => $req->email,
+                 'password' => Hash::make($req->password),
+               ]);
+               $token = $user->createToken($user->email.'_token')->plainTextToken;
+               return response()->json([
+                'status' => 200,
+                'username' => $user->name,
+                'token' =>$token,
+                'message' => 'registered Sucessfully',
+               ]);
+               
+            }
+    }
+
+
+
+    public function login(Request $req){
+
+        $validator = Validator::make($req->all(), [
+            'email'=> 'required|email|max:190',
+            'password' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'validation_errors' => $validator->messages(),
+            ]);
+        }else{
+            $user = User::where('email', $req->email)->first();
+ 
+            if (! $user || ! Hash::check($req->password, $user->password)) {
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'Invalid Credential',
+                   ]);
+            }else{
+                $token = $user->createToken($user->email.'_token')->plainTextToken;
+                return response()->json([
+                 'status' => 200,
+                 'username' => $user->name,
+                 'token' =>$token,
+                 'message' => 'logged in Sucessfully',
+                ]);
+            }
+        }
+
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'status' => 200,
+            'message' => 'logged out Sucessfully',
+           ]);
+    }
+}
---
+++ front_end/src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import MasterLayout from "./layouts/admin/MasterLayout";
+import Dashboard from "./components/admin/Dashboard";
+import Profile from "./components/admin/Profile";
+import Home from "./components/frontend/Home";
+import Login from "./components/frontend/auth/Login";
+import Register from "./components/frontend/auth/Register";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+axios.defaults.withCredentials = true;
+
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('auth_token');
+  config.headers.Authorization = token ? `Bearer ${token}` : '';
+  return config;
+});
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* nested routes */}
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+
+
+
+
+          <Route path="/admin" element={<MasterLayout />} >
+            <Route path='/admin/dashboard' element={<Dashboard />} />
+            <Route path='/admin/profile' element={<Profile />} />
+            <Route index element={<Navigate to="/admin/dashboard" />} />
+          </Route>
+
+
+
+        </Routes>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
